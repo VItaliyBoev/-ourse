31fa136c716eb803ebef6e1ecc23268d5d1abfa4
VItaliyBoev <81472884+VItaliyBoev@users.noreply.github.com>
2021-03-27 19:45:59 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="ajax.js"></script>
+</head>
+<body>
+<h1>Проверьте является ли год високосным</h1>
+<form name = "form"  action ="add.php" method ="post">
+    <p><input name = "name" type = "text" placeholder = "Введите год..."/></p>
+    <p><button  type="submit"  class="" >Проверить</button></p>
+</form>
+</body>
+</html>
